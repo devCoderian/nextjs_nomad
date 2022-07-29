878412b96a382cd64684728236de2a2ed4086c62
devcoderian <devcoderian@gmail.com>
2022-07-29 13:59:23 +0900

    FrameWork Overview 확인

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,16 +8,42 @@ export default function Navbar(){
 
     return( 
             <nav>
-                {/* Link에 스타일을 주고 싶다면 a태그내에 넣어준다. 그걸 위해 앵커 태그가 존재한다.= 아니라면 생략 가능 */}
                <Link href ="/">
-                    <a style={{ color: router.pathname === '/'? 'red': 'blue'}}>Home</a>
+                    <a style={{ color: router.pathname === '/'? 'active': 'blue'}}>Home</a>
                </Link>
                <Link href ="/about">
-                    <a style={{ color: router.pathname === '/about'? 'red': 'blue'}}>about</a>
+                    <a style={{ color: router.pathname === '/about'? 'active': 'blue'}}>about</a>
                </Link>
                 {/* a태그를 Next에서 이동할 때 사용하지 말기 */}
                 {/* <a href = "/">Home</a>
                 <a href ="/about">About</a> */}
+                <style jsx>{`
+                    nav{
+                        background-color: tomato;
+                    }
+                    a{
+                        text-decoration: none;
+                    }
+                    .active{
+                        color: yellow;
+                    }
+                `}</style>
+                {/* globalStyles 추가하는 법 
+                    <styles jsx global>
+                    {`
+                    a{
+                        color: white;
+                        color: ${props.color}; //이런식으로 props도 사용가능
+                    }   
+                    `}
+                    </styles>
+                 이 방법의 단점 => 페이지별로 적용해주어야 함.
+
+                 App component 사용하기
+                 App component는 모든 컴포넌트의 청사진 => next js 프레임워크 제공 기능
+                 //_app.js 생성
+                //Next.js 가 가장 먼저 바라보는 파일 _app.js
+                 */}
             </nav>
     )
 }
---
+++ pages/_app.js
@@ -0,0 +1,48 @@
+import NavBar from '../components/NavBar'
+import "../styles/globals.css"; //컴포넌트 X, _app.js에서만 가능하다.
+// import { global } from 'styled-jsx/css';
+
+//App => 이름 변경 상관없지만 _app.js 는 필수값
+export default function App({Component, pageProps}){ //Prop 두개 고정/ Component, pageProps
+    return (
+            <>
+                <NavBar />
+                <Component {...pageProps} />
+                <style jsx global>{`
+                        a {
+                            color: white;
+                        }
+                    `}
+                </style>
+            </>
+        );
+}
+// + 파일명.module.css 파일 형태를 제외한 모든 나머지 css파일들은 _app.js에서만 import해와서 사용해야 한다. (글로벌 css간의 충돌을 피하기 위해서이다.)
+// https://nextjs.org/docs/messages/css-global
+
+ /*
+        span이 모든 페이지에 추가된다.
+        return (
+            <div>
+                <Component {...pageProps} />;
+                <span>hello</span> 
+            </div>
+        );
+    */
+
+    
+/**
+ * rehydration이란?
+ * Next.js가 백엔드상에서 React.js를 돌리고 있고
+ * NextJS가 페이지를 pre-generate(사전 생성) => HTML 페이지가 생성된다.
+ * 하지만 유저가 모든 자바스크립트를 다운로드 한 후에,
+ * ReactJS가 다시 주도권을 가져와서 일반적인 ReactJS 처럼 동작한다.
+ * js가 생성되면 그후 주도권 Reactjs
+ */
+
+
+/**
+ * XXX.module.css(.nav {})
+ * import styles from "./XXXX.module.css"
+ * <div className = {styles.nav} />
+ */
